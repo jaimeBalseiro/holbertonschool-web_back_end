fc1efdbccf59e46f46f51348e9f2066a6ab75020
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-05-24 09:43:16 -0400

    done

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a function to str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    n as argument returns
+    the str representation
+    of the float
+    """
+    return str(n)
