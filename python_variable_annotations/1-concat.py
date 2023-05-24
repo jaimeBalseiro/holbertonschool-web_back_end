bd8c21871b84148b71182c420b0ffc21fe95e777
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-05-24 09:26:02 -0400

    done

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Write a function that takes two strings
+and sums them and returns
+a concatenated string
+"""
+
+
+def concat(str1: float, str2: float) -> float:
+    """
+    Func takes the two strings
+    and returns the sum as a 
+    concat string
+    """
+    return str1 + str2
