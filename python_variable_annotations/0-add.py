78d05ded21a93fc408e93f42246d37db078e972f
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-05-24 09:12:43 -0400

    made the documentation for the 'add' func and fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function add, that takes a float a, b and returns their sum"""
+"""
+Write a type-annotated function add, that 
+takes a float a, b and returns their sum
+"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    function that adds the floats and makes the sum
+    """
     return a + b
