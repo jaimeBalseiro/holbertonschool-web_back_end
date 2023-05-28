d2a4b4e3e2e62a4c60b72b953613acc5795a6d4c
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-05-28 17:23:29 -0400

    test

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Create a class BasicCache that inherits from
+BaseCaching and is a caching system '''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' Basic dictionary '''
+
+    def put(self, key, item):
+        ''' insert element into the dict '''
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' get element from dict '''
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
