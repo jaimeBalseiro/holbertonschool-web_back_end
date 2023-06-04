f5c18c972e565f0d31be10403b6bcb108f187c82
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-06-04 18:36:05 -0400

    blank line fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ import os
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ returns the log message obfuscated """
