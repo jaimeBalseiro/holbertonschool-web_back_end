36ac6c1c4f706c269b5a98f08ac99e13dba67bf5
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-06-10 16:29:04 -0400

    done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -69,3 +69,15 @@ class Auth:
         except NoResultFound:
             return False
         return checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """
+        Create new session and return the session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return
