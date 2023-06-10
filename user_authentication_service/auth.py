bdd96dfc91a0ad83e8ee5353f7d73583847371c7
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-06-10 15:39:57 -0400

    done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -20,3 +20,26 @@ def _hash_password(password: str) -> str:
         str: password hashed
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a user
+
+        Args:
+            email (str): email of user
+            password (str): password of user
+
+        Returns:
+            User: user registered
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
