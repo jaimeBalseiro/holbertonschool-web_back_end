0cc277292748cfdf21e27de60b35652b44c1d254
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-06-11 14:56:07 -0400

    done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -110,3 +110,24 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """get reset password token
+
+        Args:
+            email (str): user email
+
+        Raises:
+            ValueError: if not found user
+
+        Returns:
+            str: reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
