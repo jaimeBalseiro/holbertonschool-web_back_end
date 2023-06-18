c811acc5562188d2796a6ed6a32149737ed33e9a
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-06-18 16:28:47 -0400

    test 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import unittest
 from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
+from unittest import TestCase, mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,24 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertRaises(ex)
 
 
-class TestGetJson(unittest.TestCase):
-    """
-    TestGetJson class
-    """
+class TestGetJson(TestCase):
+    """ Class for testing get_json function """
+    # order of args: test_url, test_payload
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    def test_get_json(self, url, payload):
-        """
-        test get_json method
-        """
-        mock_response = mock.Mock()
-        mock_response.json.return_value = payload
-
-        with mock.patch('requests.get', return_value=mock_response):
-            self.assertEqual(get_json(url), payload)
+    def test_get_json(self, test_url, test_payload):
+        """ Test method returns correct output """
+        # set mock response to have return value of test payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        # function calls requests.get, need patch to get mock return value
+        with patch('requests.get', return_value=mock_response):
+            real_response = get_json(test_url)
+            self.assertEqual(real_response, test_payload)
+            # check that mocked method called once per input
             mock_response.json.assert_called_once()
+
 
 
 if __name__ == '__main__':
