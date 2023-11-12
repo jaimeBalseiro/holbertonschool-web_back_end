767786bc81d3776363a69db01f9cd20fe0497250
Jaime Diaz Balseiro <105442363+jaimeBalseiro@users.noreply.github.com>
2023-11-12 17:00:02 +0000

    test

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python3
 """
-Python script that provides some stats about Nginx logs stored in MongoDB.
+Where can I learn Python?
 """
 from pymongo import MongoClient
 
-
-def log_infos() -> None:
-    """ Provides some stats about Nginx logs stored in MongoDB. """
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
-
-    print(
-        "{} logs".format(
-            nginx.count_documents({})
-        )
-    )
-
-    print("Methods:")
-
-    for method in methods:
-        print(
-            "\tmethod {}: {}".format(
-                method, nginx.count_documents({'method': method})
-            )
-        )
-
-    print(
-        "{} status check".format(
-            nginx.count_documents({'method': 'GET', 'path': '/status'})
-        )
-    )
-
-
 if __name__ == "__main__":
-    log_infos()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    number = nginx_collection.count()
+    number_get = nginx_collection.find({"method": "GET"}).count()
+    number_post = nginx_collection.find({"method": "POST"}).count()
+    number_put = nginx_collection.find({"method": "PUT"}).count()
+    number_patch = nginx_collection.find({"method": "PATCH"}).count()
+    number_delete = nginx_collection.find({"method": "DELETE"}).count()
+    number_status = nginx_collection.find(
+        {"method": "GET", "path": "/status"}).count()
+
+    print("{} logs".format(number))
+    print("Methods:")
+    print("\tmethod GET: {}".format(number_get))
+    print("\tmethod POST: {}".format(number_post))
+    print("\tmethod PUT: {}".format(number_put))
+    print("\tmethod PATCH: {}".format(number_patch))
+    print("\tmethod DELETE: {}".format(number_delete))
+    print("{} status check".format(number_status))
